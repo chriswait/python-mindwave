bfb66d46b0045e1c501215c9aa205cb181476ff5
Frank Ellis <silleknarf@gmail.com>
2013-04-19 04:54:41 +0100

    Cleaning up the visualiser code, still needs tested using the device

--- visualiser.py
+++ visualiser.py
@@ -5,15 +5,46 @@ from pygame.locals import *
 from MindwaveDataPoints import *
 from MindwaveDataPointReader import MindwaveDataPointReader
 
-pygame.init()
-pygame.font.init()
-screen = pygame.display.set_mode((1960, 1080))
-pygame.display.set_caption('MindWave')
+class Point:
+    def __init__(self):
+        self.meditation = 0
+        self.attention = 0
+        self.noise = 0
 
-myfont = pygame.font.SysFont(None, 56)
+        self.delta = 0
+        self.theta = 0
+        self.lowAlpha = 0
+        self.highAlpha = 0
+        self.lowBeta = 0
+        self.highBeta = 0
+        self.lowGamma = 0
+        self.midGamma = 0
 
-class Point():
-    pass
+    def process(self, dataPoint):
+        if (not dataPoint.__class__ is RawDataPoint):
+            if isinstance(dataPoint, MeditationDataPoint):
+                #print "Meditation " , dataPoint.meditationValue
+                self.meditation = int(dataPoint.meditationValue)
+                #lastMeditation = self.meditation
+            elif isinstance(dataPoint, AttentionDataPoint):
+                #selfrint "Attetntion " , dataPoint.attentionValue
+                self.attention = int(dataPoint.attentionValue)
+                #lastAttention = self.attention
+
+            elif isinstance(dataPoint, EEGPowersDataPoint):
+                #selfrint "EEGPowers"
+                self.delta = dataPoint.delta
+                self.theta = dataPoint.theta
+                self.lowAlpha = dataPoint.lowAlpha
+                self.highAlpha = dataPoint.highAlpha
+                self.lowBeta = dataPoint.lowBeta
+                self.highBeta = dataPoint.highBeta
+                self.lowGamma = dataPoint.lowGamma
+                self.midGamma = dataPoint.midGamma
+            elif isinstance(dataPoint, PoorSignalLevelDataPoint):
+                #selfrint "NoiseDataPoint" , dataPoint.amountOfNoise
+                self.noise = int(dataPoint.amountOfNoise)
+                #lastNoise = self.noise
 
 class Circle:
     def __init__(self, pos, colour, smoothingConstant=0.25):
@@ -23,17 +54,52 @@ class Circle:
         self.prevLevel = 0.0
         self.smoothingConstant = smoothingConstant
 
-    def update(self, level):
-        self.prevLevel = self.level
-        self.level = ((1-self.smoothingConstant) * self.prevLevel) + (self.smoothingConstant * level)
+    def update(self, point):
+        level = self.getLevel(point)
+        if level:
+            self.prevLevel = self.level
+            self.level = ((1-self.smoothingConstant) * self.prevLevel) + (self.smoothingConstant * level)
 
     def render(self, background):
         level = float(self.level)/100
         pygame.draw.circle(background, (0, 0, 0), self.position, 500, 1)
         pygame.draw.circle(background, self.colour, self.position, int(500*level), 0)
 
+class MeditationCircle(Circle):
+    def getLevel(self, point):
+        if hasattr(point, 'meditation'):
+            return point.meditation
+
+class AttentionCircle(Circle):
+    def getLevel(self, point):
+        if hasattr(point, 'attention'):
+            return point.attention
+
+class NoiseIndicator:
+    def __init__(self,fontSize):
+        self.myfont = pygame.font.SysFont(None, fontSize)
+        self.noise = 0
+
+    def update(self, point):
+        if hasattr(point, 'noise'):
+            self.noise = point.noise
+
+    def render(self, background):
+        # Draw the noise level if there is noise
+        if noise > 0:
+            label = self.myfont.render("Noise: "+str(self.noise), 1, (0,0,0))
+            print "Noise:", self.noise
+            background.blit(label, (10, 10))
+
 
 if __name__ == '__main__':
+    # Setup pygame
+    pygame.init()
+    pygame.font.init()
+    screen = pygame.display.set_mode((1960, 1080))
+    pygame.display.set_caption('MindWave')
+
+    # Setup mindwave
     mindwaveDataPointReader = MindwaveDataPointReader()
     mindwaveDataPointReader.start()
 
@@ -43,60 +109,46 @@ if __name__ == '__main__':
 
     # Create a circle for attention
     red = (255,0,0)
-    attentionCircle = Circle(third, red)
+    attentionCircle = AttentionCircle(third, red)
 
     # Create a circle for meditation
     blue = (0, 255, 0)
-    meditationCircle = Circle(secondThird, blue)
+    meditationCircle = MeditationCircle(secondThird, blue)
 
-    lastMeditation = 0
-    lastAttention = 0
+    # Create noise indicator in the top left
+    fontSize = 56
+    noiseIndicator = NoiseIndicator(fontSize)
 
-    background = pygame.Surface(screen.get_size())
+    # Set up the UI elements
+    ui = [attentionCircle, meditationCircle, noiseIndicator]
+    p = Point()
 
+    # Set up frame rate counter
+    fps = 30
+    lastRendered = pygame.time.get_ticks()
+
+    # Main loop
     while(True):
         dataPoint = mindwaveDataPointReader.readNextDataPoint()
-        if (not dataPoint.__class__ is RawDataPoint):
+        p.process(dataPoint)
+
+        for element in ui:
+            element.update(p)
+
+        # Limit the frame rate
+        timeNow = pygame.time.get_ticks()
+        if timeNow > (lastRendered+(1000/fps)):
+
             # Draw the background
             background = pygame.Surface(screen.get_size())
             background = background.convert()
             background.fill((250, 250, 250))
-            p = Point()
-            if isinstance(dataPoint, MeditationDataPoint):
-                #print "Meditation " , dataPoint.meditationValue
-                p.meditation = int(dataPoint.meditationValue)
-                lastMeditation = p.meditation
-            elif isinstance(dataPoint, AttentionDataPoint):
-                #print "Attetntion " , dataPoint.attentionValue
-                p.attention = int(dataPoint.attentionValue)
-                lastAttention = p.attention
-
-            elif isinstance(dataPoint, EEGPowersDataPoint):
-                #print "EEGPowers"
-                p.delta = dataPoint.delta
-                p.theta = dataPoint.theta
-                p.lowAlpha = dataPoint.lowAlpha
-                p.highAlpha = dataPoint.highAlpha
-                p.lowBeta = dataPoint.lowBeta
-                p.highBeta = dataPoint.highBeta
-                p.lowGamma = dataPoint.lowGamma
-                p.midGamma = dataPoint.midGamma
-            elif isinstance(dataPoint, PoorSignalLevelDataPoint):
-                #print "NoiseDataPoint" , dataPoint.amountOfNoise
-                p.noise = int(dataPoint.amountOfNoise)
-                # Draw the noise level if there is noise
-                if p.noise > 0:
-                    label = myfont.render("Noise: "+str(p.noise), 1, (0,0,0))
-                    print "Noise:", p.noise
-                    background.blit(label, (10, 10))
-
-            meditationCircle.update(lastMeditation)
-            attentionCircle.update(lastAttention)
 
             # Render the circles
-            attentionCircle.render(background)
-            meditationCircle.render(background)
+            for element in ui:
+                element.render(background)
+            lastRendered = timeNow
 
-            # Render everything!
+            # Blit everything!
             screen.blit(background, (0, 0))
             pygame.display.flip()
